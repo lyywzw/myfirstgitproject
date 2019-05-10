fa2389b884e5040265cc6b38f99c829e2504da59
lyywzw <50451910+lyywzw@users.noreply.github.com>
2019-05-10 20:49:13 +0800

    Update test.java

--- Firstgit/src/test.java
+++ Firstgit/src/test.java
@@ -4,6 +4,6 @@ public static void main(String[] args) {
 	System.out.println("第一次发布");
 	System.out.println("修改以后再提交");
 	String nn="dddd";
-        System.out.println("我是在服务器端修改的文件");
+        System.out.println("我是在服务器端修改的文件");//顺便制造冲突
 }
 }
